0500610540abf0a2ff2485ff8b1342b3d6822a66
vmw-pso <pstacey@vmware.com>
2023-01-02 15:25:14 +1100

    Updated schema

--- migrations/000001_create_base_tables.up.sql
+++ migrations/000001_create_base_tables.up.sql
@@ -1,98 +1,85 @@
 CREATE EXTENSION IF NOT EXISTS citext;
 
-CREATE TYPE "clearance" AS ENUM (
-  'None',
-  'Baseline',
-  'NV1',
-  'NV2',
-  'TSPV'
-);
-
-CREATE TYPE "revenue_type" AS ENUM (
-  'Fixed Fee',
-  'T&M'
-);
-
 CREATE TABLE "workgroup" (
-  "workgroup_id" bigserail PRIMARY KEY,
-  "workgroup_name" varchar,
+  "workgroup_id" bigserial PRIMARY KEY,
+  "workgroup_name" varchar NOT NULL UNIQUE,
   "description" varchar
 );
 
 CREATE TABLE "job_title" (
   "title_id" bigserial PRIMARY KEY,
-  "title" varchar,
+  "title" varchar NOT NULL UNIQUE,
   "description" varchar
 );
 
 CREATE TABLE "resource" (
   "employee_id" integer PRIMARY KEY,
-  "name" varchar,
-  "email" varchar,
-  "job_title_id" integer,
-  "manager_id" integer,
-  "workgroup_id" integer,
-  "clearance_level" clearance,
+  "name" varchar NOT NULL,
+  "email" varchar NOT NULL UNIQUE,
+  "job_title_id" integer NOT NULL,
+  "manager_id" integer NOT NULL,
+  "workgroup_id" integer NOT NULL,
+  "clearance" varchar NOT NULL CHECK (clearance = ANY(ARRAY['None', 'Baseline', 'NV1', 'NV2', 'TSPV']::varchar[])),
   "specialties" text[],
   "certifications" text[],
-  "active" boolean
+  "active" boolean NOT NULL DEFAULT 't'
 );
 
 CREATE TABLE "project_status" (
   "status_id" bigserial PRIMARY KEY,
-  "status" varchar,
+  "status" varchar NOT NULL UNIQUE,
   "description" varchar
 );
 
 CREATE TABLE "project" (
   "opportunity_id" varchar PRIMARY KEY,
-  "changepoint_id" varchar,
-  "name" varchar,
-  "revenue_type" revenue_type,
-  "customer" varchar,
+  "changepoint_id" varchar UNIQUE,
+  "name" varchar NOT NULL,
+  "revenue_type" varchar NOT NULL CHECK(revenue_type = ANY(ARRAY['Fixed Fee', 'T&M']::varchar[])),
+  "customer" varchar NOT NULL,
   "end_customer" varchar,
   "project_manager_id" int,
-  "status" varchar
+  "status_id" integer
 );
 
 CREATE TABLE "resource_request" (
   "request_id" bigserial PRIMARY KEY,
-  "opportunity_id" integer,
-  "job_title_id" integer,
+  "opportunity_id" varchar NOT NULL,
+  "job_title_id" integer NOT NULL,
   "total_hours" numeric,
-  "skills" text[],
+  "skills" text[] NOT NULL,
   "start_date" date,
   "hours_per_week" numeric,
   "status" varchar,
-  "created_at" timestamp,
-  "updated_at" timestamp,
-  "version" integer
+  "created_at" timestamp NOT NULL DEFAULT current_timestamp,
+  "updated_at" timestamp NOT NULL DEFAULT current_timestamp,
+  "version" integer NOT NULL DEFAULT 1
 );
 
 CREATE TABLE "resource_request_comment" (
   "comment_id" bigserial PRIMARY KEY,
-  "request_id" integer,
-  "comment" varchar,
-  "created_at" timestamp,
-  "updated_at" timestamp,
-  "version" integer
+  "request_id" integer NOT NULL,
+  "comment" varchar NOT NULL,
+  "created_at" timestamp NOT NULL DEFAULT current_timestamp,
+  "updated_at" timestamp NOT NULL DEFAULT current_timestamp,
+  "version" integer NOT NULL DEFAULT 1
 );
 
 CREATE TABLE "resource_assignment" (
   "assignment_id" bigserial PRIMARY KEY,
-  "employee_id" integer,
-  "resource_request_id" integer,
-  "start_date" date,
+  "resource_request_id" integer NOT NULL,
+  "employee_id" integer NOT NULL,
+  "start_date" date NOT NULL,
   "end_date" date,
-  "hours_per_week" numeric
+  "hours_per_week" numeric NOT NULL
 );
 
 CREATE TABLE "new_hire" (
   "requirement_id" bigserial PRIMARY KEY,
-  "resource_request_id" integer,
-  "description" varchar,
-  "created_at" timestamp,
-  "filled" boolean,
+  "resource_request_id" integer NOT NULL,
+  "description" varchar NOT NULL,
+  "created_at" timestamp NOT NULL DEFAULT current_timestamp,
+  "filled" boolean NOT NULL DEFAULT 'f',
   "status" varchar
 );
 
@@ -100,29 +87,29 @@ CREATE TABLE "new_hire_update" (
   "update_id" bigserial PRIMARY KEY,
   "requirement_id" integer,
   "comment" varchar,
-  "created_at" timestamp,
-  "updated_at" timestamp,
-  "version" integer
+  "created_at" timestamp NOT NULL DEFAULT current_timestamp,
+  "updated_at" timestamp NOT NULL DEFAULT current_timestamp,
+  "version" integer NOT NULL DEFAULT 1
 );
 
 ALTER TABLE "resource" ADD FOREIGN KEY ("workgroup_id") REFERENCES "workgroup" ("workgroup_id");
 
 ALTER TABLE "resource" ADD FOREIGN KEY ("job_title_id") REFERENCES "job_title" ("title_id");
 
+ALTER TABLE "resource" ADD FOREIGN KEY ("manager_id") REFERENCES "resource" ("employee_id");
+
+ALTER TABLE "project" ADD FOREIGN KEY ("status_id") REFERENCES "project_status" ("status_id");
+
 ALTER TABLE "resource_request" ADD FOREIGN KEY ("opportunity_id") REFERENCES "project" ("opportunity_id");
 
-ALTER TABLE "project" ADD FOREIGN KEY ("status") REFERENCES "project_status" ("status");
+ALTER TABLE "resource_request" ADD FOREIGN KEY ("job_title_id") REFERENCES "job_title" ("title_id");
 
-ALTER TABLE "new_hire" ADD FOREIGN KEY ("requirement_id") REFERENCES "resource_request" ("request_id");
+ALTER TABLE "resource_request_comment" ADD FOREIGN KEY ("request_id") REFERENCES "resource_request" ("request_id");
+
+ALTER TABLE "new_hire" ADD FOREIGN KEY ("resource_request_id") REFERENCES "resource_request" ("request_id");
 
 ALTER TABLE "new_hire_update" ADD FOREIGN KEY ("requirement_id") REFERENCES "new_hire" ("requirement_id");
 
 ALTER TABLE "resource_assignment" ADD FOREIGN KEY ("employee_id") REFERENCES "resource" ("employee_id");
 
-ALTER TABLE "resource_assignment" ADD FOREIGN KEY ("resource_request_id") REFERENCES "resource_request" ("opportunity_id");
-
-ALTER TABLE "resource" ADD FOREIGN KEY ("manager_id") REFERENCES "resource" ("employee_id");
-
-ALTER TABLE "resource_request" ADD FOREIGN KEY ("job_title_id") REFERENCES "job_title" ("title_id");
-
-ALTER TABLE "resource_request_comment" ADD FOREIGN KEY ("request_id") REFERENCES "resource_request" ("request_id");
+ALTER TABLE "resource_assignment" ADD FOREIGN KEY ("resource_request_id") REFERENCES "resource_request" ("request_id");
